c672bbe68158f0b39b8a182a9efdd99ed4e40041
hideto13 <pinki_00@mail.ru>
2021-09-06 16:36:41 +0300

    Change token test

--- test/token-test.js
+++ test/token-test.js
@@ -1,13 +1,26 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("MyToken", function () {
-  it("Should return zero balance", async function () {
-    const MyToken = await ethers.getContractFactory("MyToken");
-    const token = await MyToken.deploy();
-    await token.deployed();
+describe("MyToken", function() {
+	beforeEach(async function() {
+		this.Token = await ethers.getContractFactory("MyToken");
+		this.token = await this.Token.deploy();
+		await this.token.deployed();
+	});
 
-    expect(await token.totalSupply()).to.equal(0);
+	it("Should return name", async function() {
+		expect(await this.token.name()).to.equal("Very Bad Token");
 
-    });
+	});
+	it("Should return symbol", async function() {
+		expect(await this.token.symbol()).to.equal("VBT");
+
+	});
+	it("Should return decimals", async function() {
+		expect(await this.token.decimals()).to.equal(2);
+	});
+    it("Should return zero balance", async function() {
+		expect(await this.token.totalSupply()).to.equal(0);
+
+	});
 });
